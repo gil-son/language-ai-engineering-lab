b486923d3a3f6408e2120a6430e0d8a4baa55840
gil-son <gilszon@gmail.com>
2026-01-29 19:56:05 -0300

    feat: type of prompts - CoT self consistency

---
+++ projects/11-LLM-Orchestration/langchain/ollama/6_types_of_prompts/4.2_CoT_Self_consistency.py
@@ -0,0 +1,22 @@
+from langchain_ollama import ChatOllama
+from utils import print_llm_result
+
+msg1 = """
+Question: In an API endpoint that returns a list of users and their posts, the developer wrote:
+
+users := db.FindAllUsers()
+for _, u := range users {
+    u.Posts = db.FindPostsByUserID(u.ID)
+}
+
+How many database queries will this code execute if there are N users?
+
+Generate 3 different reasoning paths step by step.
+At the end, summarize the answers and choose the most consistent one, ignoring outliers.
+If there are 3 different answers, ONLY reply: "I can't find a consistent answer".
+"""
+
+llm = ChatOllama(model="llama3.2")
+
+response1 = llm.invoke(msg1)
+print_llm_result(msg1, response1)
